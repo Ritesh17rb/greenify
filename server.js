928e96b8e4538f3fdf0f7f0890f31faa0c26db02
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-27 18:31:47 +0530

    new cors

--- server.js
+++ server.js
@@ -1,59 +1,48 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cors from 'cors';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 // Configure env
-dotenv.config();
+dotenv.config()
 
-// Database config
+// database config
 connectDB();
 
-// Express app
-const app = express();
+// Rest object
+const app = express()
 
-// Middleware
-// app.use(
-//   cors({
-//     origin: '*',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//   })
-// );
+// middlewares
 // CORS middleware
-app.use(
-  cors({
-    origin: 'https://greenify-2ohq.vercel.app', // Adjust to your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'https://greenify-2ohq.vercel.app', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+}));
 
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.json())
+app.use(morgan('dev'))
 
-// Routes
+// routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
+app.use("/api/v1/product", productRoutes);
 
-// Test route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: 'Welcome to green delight',
-  });
-});
+    message: "Welcome to green delight"
+  })
+})
 
-// Port configuration
-const PORT = process.env.PORT || 8080;
+// PORT
+const PORT = process.env.PORT || 8080
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-});
+})
