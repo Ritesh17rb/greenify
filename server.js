1f61cbdff1e074d422ada31fcf9af1b0af010c61
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-27 17:29:03 +0530

    cors

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import cors from "cors";
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
 
 // Configure env
-dotenv.config()
+dotenv.config();
 
-// database config
+// Database config
 connectDB();
 
-// Rest object
-const app = express()
+// Express app
+const app = express();
 
-// middlewares
+// Middleware
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-})
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
 );
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan('dev'));
 
-// routes
+// Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use('/api/v1/product', productRoutes);
 
-app.get("/",(req,res)=>{
-   res.send({
-    message:"Welcome to green delight"
-   })
-})
+// Test route
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Welcome to green delight',
+  });
+});
 
-// PORT
-const PORT = process.env.PORT || 8080
+// Port configuration
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
-      console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-})
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
+});
