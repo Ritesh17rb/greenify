4409917fd717a06e073fc7d0dcd8d2394de656ed
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-28 08:51:13 +0530

    deploy

--- server.js
+++ server.js
@@ -1,56 +1,47 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cors from 'cors';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 // Configure env
-dotenv.config();
+dotenv.config()
 
-// Database config
+// database config
 connectDB();
 
 // Rest object
-const app = express();
+const app = express()
 
-// CORS middleware
-const corsOptions = {
-  origin: 'https://greenify-ashy.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true,
-};
+// middlewares
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json())
+app.use(morgan('dev'))
 
-app.use(cors(corsOptions));
-
-
-app.use(express.json());
-app.use(morgan('dev'));
-
-// Routes
+// routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
+app.use("/api/v1/product", productRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: 'Welcome to green delight',
-  });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack); // Log the error stack trace
-  res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
-});
+    message: "Welcome to green delight"
+  })
+})
 
 // PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-});
+})
