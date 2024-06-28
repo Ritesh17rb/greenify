8e57541fd913ca3c51e701983b87832a33ed669c
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-28 07:05:12 +0530

    new new push

--- server.js
+++ server.js
@@ -1,48 +1,53 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import cors from "cors";
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
 
 // Configure env
-dotenv.config()
+dotenv.config();
 
 // database config
 connectDB();
 
 // Rest object
-const app = express()
+const app = express();
 
-// middlewares
 // CORS middleware
 app.use(cors({
-  origin: '*', // Allow requests from any origin
+  origin: 'https://greenify-2ohq.vercel.app', // Specify your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
 }));
 
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan('dev'));
 
 // routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use('/api/v1/product', productRoutes);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send({
-    message: "Welcome to green delight"
-  })
-})
+    message: 'Welcome to green delight',
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error stack trace
+  res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
+});
 
 // PORT
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT : ${PORT}`.bgCyan.white);
-})
+});
